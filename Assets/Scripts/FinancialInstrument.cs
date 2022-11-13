2c3f412f229ad887c7f70c92d4291d633433536d
Dong Ok Park <ehd85623@gmail.com>
2022-11-13 20:17:04 +0900

    [DT-76] FinancialInstrument 클래스 추가

---
+++ Assets/Scripts/FinancialInstrument.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class FinancialInstrument
+{
+    private int code;
+    private string name;
+    private double interest;
+    private double deposit;
+}
